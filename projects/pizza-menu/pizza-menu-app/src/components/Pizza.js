f4b394966e5b40aff77b2f84aa9063845b37fdb3
suresh.dharisi <suresh.dharisi@gmail.com>
2024-12-21 15:28:41 -0500

    Added supporting file to pizza menu app

---
+++ projects/pizza-menu/pizza-menu-app/src/components/Pizza.js
@@ -0,0 +1,16 @@
+import {pizzaData} from '../testdata/data.js';
+
+function Pizza(){
+    
+    return (
+    <div>
+        <img src='pizzas/spinaci.jpg' alt='Pizza Spinaci'></img>
+        <h2>Pizza Spinaci</h2>
+        <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+    
+    );
+}
+
+
+export default Pizza;
